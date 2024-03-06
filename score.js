2ac56d59e2b374d044094abc94256d7c5343a4de
rapolurenu <159129847+rapolurenu@users.noreply.github.com>
2024-03-06 17:21:20 +0530

    Update score.js

--- score.js
+++ score.js
@@ -1,1 +1,10 @@
+const playAgainBtn = document.getElementById("play-again-button");
+const scoreBoard = document.getElementById("score-board");
+
+if (playAgainBtn)
+  playAgainBtn.onclick = function () {
+    window.location.href = "./index.html";
+  };
+
 // Iteration 5: Store the player score and display it on the game over screen
+if (scoreBoard) scoreBoard.innerText = localStorage.getItem("score");
